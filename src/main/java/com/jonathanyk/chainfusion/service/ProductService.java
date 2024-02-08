e729c7701d28d8eeb1647dc2cb7b230b6601ac03
Yonatan Kalma <yonatan.kalma@gmail.com>
2024-02-08 03:37:34 +0200

    fixed stashes

--- src/main/java/com/jonathanyk/chainfusion/service/ProductService.java
+++ src/main/java/com/jonathanyk/chainfusion/service/ProductService.java
@@ -1,50 +1,3 @@
-<<<<<<< Updated upstream
-package com.jonathanyk.chainfusion.service;
-
-import com.jonathanyk.chainfusion.dto.ProductRequest;
-import com.jonathanyk.chainfusion.dto.ProductResponse;
-import com.jonathanyk.chainfusion.model.Product;
-import com.jonathanyk.chainfusion.repository.ProductRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-@Service
-@RequiredArgsConstructor
-@Slf4j
-public class ProductService {
-
-    private final ProductRepository productRepository;
-
-    public void createProduct(ProductRequest productRequest) {
-        Product product = Product.builder()
-                .name(productRequest.getName())
-                .description(productRequest.getDescription())
-                .price(productRequest.getPrice())
-                .build();
-
-        productRepository.save(product);
-        log.info("product id: {} saved", product.getId());
-    }
-
-    public List<ProductResponse> getAllProducts() {
-        List<Product> allProducts = productRepository.findAll();
-        return allProducts.stream().map(product -> mapToProductResponse(product)).toList();
-    }
-
-    private ProductResponse mapToProductResponse(Product product) {
-        return ProductResponse.builder()
-                .id(product.getId())
-                .name(product.getName())
-                .description(product.getDescription())
-                .price(product.getPrice())
-                .build();
-    }
-}
-=======
 package com.jonathanyk.chainfusion.service;
 
 import com.jonathanyk.chainfusion.dto.ProductRequest;
@@ -101,4 +54,3 @@ public class ProductService implements IProductService {
                 .build();
     }
 }
->>>>>>> Stashed changes
--- src/test/java/com/jonathanyk/chainfusion/ChainfusionApplicationTests.java
+++ src/test/java/com/jonathanyk/chainfusion/ChainfusionApplicationTests.java
@@ -1,19 +1,8 @@
 package com.jonathanyk.chainfusion;
 
-<<<<<<< Updated upstream
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ChainfusionApplicationTests {
-
-	@Test
-	void contextLoads() {
-=======
 import com.jonathanyk.chainfusion.dto.ProductRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
@@ -22,11 +11,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.shaded.com.github.dockerjava.core.MediaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.math.BigDecimal;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -35,7 +22,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @Testcontainers
 @AutoConfigureMockMvc
 class ChainfusionApplicationTests {
-
 
 	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:5.0.13")
 			.waitingFor(Wait.forListeningPort());
@@ -68,7 +54,7 @@ class ChainfusionApplicationTests {
 				.description("new with tags")
 				.price(BigDecimal.valueOf(100))
 				.build();
->>>>>>> Stashed changes
+
 	}
 
 }
